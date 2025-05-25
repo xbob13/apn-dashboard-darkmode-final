d7b4e3d1447ef3812e2d704093f750296cd8cb3d
xbob13 <xbob.13.1985@gmail.com>
2025-05-25 11:56:55 -0700

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Dashboard from "./Dashboard";
-import "./index.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Dashboard />);
